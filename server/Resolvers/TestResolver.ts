1c9613fbc6b1da757180bb3d98627ee609b4cbbd
Jordan L <jordanscast@gmail.com>
2021-03-08 20:25:38 -0600

    Express graphql setup (#3)
    * Add basic graphql server
    
    * 0.1.1
    
    * Linting

---
+++ server/Resolvers/TestResolver.ts
@@ -0,0 +1,9 @@
+import { Query, Resolver, Int } from "type-graphql";
+
+@Resolver()
+export class TestResolver {
+  @Query(type => Int)
+  theMeaningOfLife(): number {
+    return 42;
+  }
+}
--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello World")
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import express from 'express';
+import dotenv from 'dotenv';
+
+import { TestResolver } from './Resolvers/TestResolver';
+
+dotenv.config()
+
+const app = express();
+
+(async () => {
+  const server = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [
+        TestResolver,
+      ]
+    }),
+    introspection: true,
+    tracing: true
+  })
+
+  server.applyMiddleware({ app, path: '/api/graphql' });
+
+  app.listen(process.env.PORT, () => console.log(`Api service listening at http://localhost:${process.env.PORT} || http://localhost:${process.env.PORT}${server.graphqlPath}`))
+})()
